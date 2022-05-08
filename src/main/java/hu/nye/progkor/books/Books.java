2167cb1ccb016616b507e678c1dfceb1ebba1cd5
katonatamas99 <78899992+katonatamas99@users.noreply.github.com>
2022-05-08 22:25:51 +0200

    .

---
+++ src/main/java/hu/nye/progkor/books/Books.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Books {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Books.class, args);
+    }
+
+}
